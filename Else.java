f62d850b57fdd1fb7192b574250c761f7a9868e7
Hariprasath2706 <hariprasaths270606@gmail.com>
2026-03-21 13:23:38 +0530

    Create Else.java

---
+++ Else.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args){
+        int x = 11;
+        int y = 22;
+        int z = 33;
+        if(x>=y && y>=z){
+            System.out.print("True");
+        }
+        else{
+            System.out.print("False");
+        }
+    }
+}
