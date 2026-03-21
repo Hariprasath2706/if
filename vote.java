3744a51a1dacf2e2d086111b08173681905186f5
Hariprasath2706 <hariprasaths270606@gmail.com>
2026-03-21 13:43:23 +0530

    Create vote.java

---
+++ vote.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args){
+        int age = 19;
+        if(age > 18){
+            System.out.print("Eligible to vote");
+        }
+        else if(age < 10){
+            System.out.print("Not eligible to vote");
+        }
+    }
+}
