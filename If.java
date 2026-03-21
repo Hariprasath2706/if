55bde17832308dbf6c14a12e77270a083e3fba8a
Hariprasath2706 <hariprasaths270606@gmail.com>
2026-03-21 13:20:21 +0530

    Create If.java

---
+++ If.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+        int x = 11;
+        int y = 22;
+        int z = 33;
+        if(x<=y && y<=z){
+            System.out.print("True");
+        }
+    }
+}
