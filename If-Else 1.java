52c80ffd8f5eaf811a1f33883b84ab616366316c
Hariprasath2706 <hariprasaths270606@gmail.com>
2026-03-21 13:32:51 +0530

    Create If-Else 1.java

---
+++ If-Else 1.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static void main(String[] args){
+        int x = 11;
+        int y = 22;
+        int z = 33;
+        if(x>=y){
+            System.out.print("x is greater than y");
+        }
+        else if(y<=z){
+            System.out.print("y is lesser than z");
+        }
+        else{
+            System.out.print("z is greater than both x nd y");
+        }
+    }
+}
