30acd6a0d7248a8f90270778cc3a64f711834202
Hariprasath2706 <hariprasaths270606@gmail.com>
2026-03-21 13:50:29 +0530

    Create Nested if.java

---
+++ Nested if.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args){
+        int x = 19;
+        int y = 20;
+        if(x < y && y < x){
+            System.out.print("x is lesser ");
+        }
+        if(x < y && y > x){
+            System.out.print("x and y are greater");
+        }
+    }
+}
